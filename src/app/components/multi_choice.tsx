555e85c2bfca48fd9e9d69530f3f8c1156b2905b
leeminam32 <leeminam32@gmail.com>
2023-12-24 22:41:56 +0900

    ignore .idea folder

---
+++ src/app/components/multi_choice.tsx
@@ -0,0 +1,11 @@
+export default function MultiChoice(){
+    return (
+        <>
+            <div className="bg-gradient-to-br from-green-400 to-blue-600 flex w-screen h-screen">
+                <div className="flex justify-center items-center w-full bg-slate-200 opacity-70 m-20 rounded-3xl shadow-2xl">
+                    <div className="text-6xl text-gray-600 font-light">무슨 생각 하셨나요?</div>
+                </div>
+            </div>
+        </>
+    );
+}
